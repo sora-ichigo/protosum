9dab955b1d736efb667b5039a194bbbb770360a8
igsr5 <i@gsr5.dev>
2022-04-03 22:41:05 +0900

    debug igsr5hoge

---
+++ igsr5hoge/lib/igsr5hoge.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "igsr5hoge/version"
+
+module Igsr5hoge
+  class Error < StandardError; end
+  # Your code goes here...
+
+  def hello
+    p "hello"
+  end
+end
---
+++ igsr5hoge/lib/igsr5hoge/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Igsr5hoge
+  VERSION = "0.1.0"
+end
